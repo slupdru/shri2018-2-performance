2e8804ea4ba4bfb16c4388f36ff26ee07c98e0ca
Vyacheslav <dryuchin@group-ib.ru>
2018-10-30 09:09:05 +0300

    delete font

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+/* global require, process, module, __dirname */
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const inProduction = process.env.NODE_ENV === 'production';
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const plugins = [
+  new MiniCssExtractPlugin({
+    filename: '[name].css',
+    chunkFilename: '[id].css',
+  }),
+];
+let optimization={};
+if (inProduction) {
+  optimization = {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true,
+        },
+        sourceMap: true,
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ],
+  };
+}
+
+module.exports = {
+  plugins,
+  optimization,
+  watch: !inProduction,
+  devtool: !inProduction && 'source-map',
+  entry: {
+    main: './scripts.js',
+},
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': `${__dirname}`,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+          test: /\.css$/,
+          use: [
+            // !inProduction ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'style-loader',
+            'css-loader',
+          ],
+        },
+      {
+        test: /\.(png|svg|jpg|gif|webp|woff2)$/,
+        include: path.join(__dirname),
+        exclude: /(node_modules)/,
+        loader: 'url-loader',
+    },
+    ],
+  },
+};
